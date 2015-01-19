b7d43e8d6e3a20b1cc0edcdac9edb54c67dc7fe8
Robert Barisic <r.barisic@drooms.com>
2015-01-19 10:29:16 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GameCodes::Application.config.secret_key_base = '9383e106157fb7bd4a9f99625d54b2cf3cd19ee9f10a59d910f34286675dce686ff1970a4b4947d709aab9653de10b703cf6a99e57b2f1bb5cf8c239b51b1fb1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GameCodes::Application.config.session_store :cookie_store, key: '_game_codes_session'
