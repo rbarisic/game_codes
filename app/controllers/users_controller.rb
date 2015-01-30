bf71576a9a2561bd8b43961cc282ac58bc0a7ac4
Robert Barisic <r.barisic@drooms.com>
2015-01-30 10:19:12 +0100

    cleaned up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 	before_filter :authenticate_user!, only: [:index]
 	
 	def index
-		@users = User.paginate(:page => params[:page], :per_page => 30)
+		@users = User.paginate(:page => params[:page], :per_page => 25)
 	end
 
 	def show
